9ee9a61d61ea73c408c909948c48fcb57bd31beb
m1172 <muhshuh2000@gmail.com>
2022-11-29 00:01:03 +0100

    fully done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-import Details from './components/Details';
-import Cart from './components/Cart/Cart';
-import Default from './components/Default';
-import Modal from './components/Modal';
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Default from './components/Default'
+import Cart from './components/Cart'
+import Modal from './components/modal'
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={ProductList} />
-          <Route path='/details' component={Details} />
-          <Route path='/cart' component={Cart} />
-          <Route component={Default} />
-        </Switch>
-        <Modal />
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+       <React.Fragment>
+         <Navbar></Navbar>
+         <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>           
+         </Switch> 
+         <Modal/>
+       </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react'
+import React,{Component} from 'react';
 import Product from './Product';
-import Title from "./Title";
-import { ProductConsumer } from "../Context"
+import Title from './Title'
+import {storeProducts} from '../data'
+import {ProductConsumer} from '../context'
 
-export default class ProductList extends Component {
-    render() {
+export default class ProductList extends Component{
 
-        return (
-            <React.Fragment> 
-                <div className="py-5 p-3">
-            <div className="conatiner">
-            <Title name="Our" title=" products"/>
-                <div className="row">
-                <ProductConsumer>
-                   { value => {
-                       return value.products.map(product => {
-                           return <Product key={product.id} product={product}/>;
-                       });
-                    }} 
-                </ProductConsumer>
-                </div>
-            </div>
-                </div>
-            </React.Fragment>
-            //  <Product />
-        );
-    }
+	render(){	
+		return(
+			<React.Fragment>
+				<div className="py-5">
+					<div className="container">
+						<Title name="our" title="products"/>
+
+						<div className="row">
+							<ProductConsumer>
+								{
+									value =>{
+								       return value.products.map(product =>{
+											return <Product key={product.id} product={product}/>
+								       })
+									}
+								}
+							</ProductConsumer>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+			
+			)
+	}
 }
