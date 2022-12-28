6f1b2d52ea7ed7b901f9a3908ff5ffa14969929c
m1172 <muhshuh2000@gmail.com>
2022-12-28 20:20:48 +0100

    Updates

---
+++ precache-manifest.07ba8b614615d6b80249398c2fb9b1f9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12d8370c62abbc6614eb498d80a33fe",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "05a51666c5b1421e4555",
+    "url": "/store/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "2e510295245413ce537e",
+    "url": "/store/static/css/main.0a36da59.chunk.css"
+  },
+  {
+    "revision": "05a51666c5b1421e4555",
+    "url": "/store/static/js/2.ad2c1111.chunk.js"
+  },
+  {
+    "revision": "a9ea822160d3b45cecbde553ad0fdd69",
+    "url": "/store/static/js/2.ad2c1111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e510295245413ce537e",
+    "url": "/store/static/js/main.43f65b84.chunk.js"
+  },
+  {
+    "revision": "739860954560ac9ef2f2",
+    "url": "/store/static/js/runtime-main.ed983adf.js"
+  },
+  {
+    "revision": "ca9005e8c512105ceba0d7a483556646",
+    "url": "/store/static/media/data.ca9005e8.jpg"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/store/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/store/static/media/product-1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/store/static/media/product-2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/store/static/media/product-3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/store/static/media/product-4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/store/static/media/product-5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/store/static/media/product-6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/store/static/media/product-7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/store/static/media/product-8.d390282f.png"
+  }
+]);
